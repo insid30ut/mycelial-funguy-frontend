b6a814e55eb467912e31e803c09b150d93111a07
insid30ut <mycelialfunguy@gmail.com>
2025-10-13 19:21:41 -0500

    feat: add @vercel/speed-insights dependency and import in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Poppins, Londrina_Solid } from 'next/font/google';
 import './globals.css';
 
